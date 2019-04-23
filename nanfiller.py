6862dceb05501eb2bcd1c3fb9876f7fdfacdebad
AineKiraboMbabazi <mbabaziainekirabo@gmail.com>
2019-04-23 13:50:29 +0200

    add files to test the pandas dataframe and series datastructures

---
+++ nanfiller.py
@@ -0,0 +1,22 @@
+import numpy as np 
+import pandas as pd 
+
+ages = pd.Series(
+    {
+        'Aidan':2,
+        'Anitah':23,
+        'Amina':25
+    }
+)
+heights = pd.Series(
+    {
+        'Aidan':3,
+        'Anitah':14,
+        
+    }
+)
+
+summary = pd.DataFrame({'Ages(yrs)':ages,'Heights(ft)':heights})
+print(summary)
+print(summary.sort_values('Heights(ft)', ascending=True))
+print(summary.fillna(0.0))
